7cfc04a66743a834bb17134d4a82ceb8e4e4e324
rainbowdashx <born_to_kill_666@hotmail.com>
2013-10-26 14:16:35 +0200

    more gz's

---
+++ mdgz.lua
@@ -0,0 +1,27 @@
+
+local MDGZ = CreateFrame("frame")
+MDGZ:SetScript("OnEvent", function(self, event, ...)
+    self[event](self, ...)
+end)
+
+MDGZ:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT");
+
+
+
+
+function MDGZ:CHAT_MSG_GUILD_ACHIEVEMENT(...)
+	local arg={...}
+	local msg="gz "
+	rnd = math.random(1,100)  
+	if (rnd <30) then
+		msg="gz "
+	else if(rnd>29 and rnd <40) then
+		msg ="ein toller erfolg, "
+	else if(rnd >39 and rnd <60)then
+		msg="gz  "
+	else
+		msg="gz "
+	end
+	print(rnd)
+    SendChatMessage(msg..arg[2],"GUILD")
+end
