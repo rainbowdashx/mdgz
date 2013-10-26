08f28c084cc66aa8b0d0124fa2f15652b0a08b55
rainbowdashx <born_to_kill_666@hotmail.com>
2013-10-26 19:36:19 +0200

    stuff

--- mdgz.lua
+++ mdgz.lua
@@ -12,15 +12,16 @@ MDGZ:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT");
 function MDGZ:CHAT_MSG_GUILD_ACHIEVEMENT(...)
 	local arg={...}
 	local msg="gz "
-	rnd = math.random(1,100)  
+	local name=arg[2]
+	local rnd = math.random(1,100)  
 	if (rnd <30) then
-		msg="gz "
-	else if(rnd>29 and rnd <40) then
-		msg ="ein toller erfolg, "
-	else if(rnd >39 and rnd <60)then
-		msg="gz  "
+		msg="das ist tolle Arbeit Freunde"
+	elseif(rnd>29 and rnd <40) then
+		msg ="ein toller erfolg, " .. name
+	elseif(rnd >39 and rnd <60)then
+		msg="gz  "..name
 	else
 		msg="gz "
 	end
-    SendChatMessage(msg..arg[2],"GUILD")
+    SendChatMessage(msg,"GUILD")
 end
