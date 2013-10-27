18055234ec730542e7903b95607b390fae8470a8
rainbowdashx <born_to_kill_666@hotmail.com>
2013-10-27 18:51:07 +0100

    weights

--- mdgz.lua
+++ mdgz.lua
@@ -1,3 +1,39 @@
+local totals=50
+function weighted_total(choices)
+	local total = 0
+	for i, v in ipairs(choices) do
+		total = total + v.weight
+	end
+	return total
+end
+
+function weighted_random_choice(choices)
+	local threshold = math.random(0, totals)
+	local last_choice
+	for i,v in ipairs(choices) do
+		threshold = threshold - v.weight
+		if threshold <= 0 then return v.msg end
+		last_choice = v.msg
+	end
+	return last_choice
+end
+
+
+local msgs={
+		{msg="das ist tolle Arbeit Freunde" , weight=10},
+		{msg="Endlich jemand, der seinen Shop versteht!", weight = 10},
+		{msg="Ich dreh durch, Klasse Typ", weight=10},
+		{msg="Eher nicht so schwer, {name}" , weight=10},
+		{msg="Gute Arbeit bald hast du Bunter eingeholt! {name}", weight=10},
+		{msg="gut gemacht {name}", weight=10},
+		{msg="gz  {name}", weight=10},
+		{msg="ein toller erfolg, {name}", weight =10},
+		{msg="Atemberaubend {name}" , weight	=10},
+		{msg="gz ", weight=10},
+		{msg="gz trotz battle battle", weight=20},
+}
+
+totals=weighted_total(msgs)
 
 local MDGZ = CreateFrame("frame")
 MDGZ:SetScript("OnEvent", function(self, event, ...)
@@ -11,29 +47,13 @@ MDGZ:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT");
 
 function MDGZ:CHAT_MSG_GUILD_ACHIEVEMENT(...)
 	local arg={...}
-	local msg="gz "
+	local msg="gz"
 	local name=arg[2]
-	local rnd = math.random(1,100)  
-	if (rnd <10) then
-		msg="das ist tolle Arbeit Freunde"
-	elseif(rnd>99) then
-		msg="Endlich jemand, der seinen Shop versteht!"		
-	elseif(rnd>94) then 
-		msg="Ich dreh durch, Klasse Typ"
-	elseif(rnd>90) then
-		msg="Eher nicht so schwer, " ..name
-	elseif(rnd>85) then 
-		msg="Gute Arbeit bald hast du Bunter eingeholt! "..name
-	elseif(rnd>60) then
-		msg="gut gemacht "..name		
-	elseif(rnd>40)then
-		msg="gz  "..name
-	elseif(rnd>30) then
-		msg ="ein toller erfolg, " .. name
-	elseif(rnd>20) then
-		msg ="Atemberaubend " .. name		
-	else
-		msg="gz "
-	end
+	local msg=weighted_random_choice(msgs)
+	msg=string.gsub(msg, "{name}", name)
     SendChatMessage(msg,"GUILD")
 end
+
+
+
+
